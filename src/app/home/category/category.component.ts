c0c43a48e889f3c000f664b622a9ac319fbb2989
Matthias Plank <matthias.plank@gmx.at>
2024-02-07 22:04:46 +0100

    update category component

--- src/app/home/category/category.component.ts
+++ src/app/home/category/category.component.ts
@@ -1,15 +1,18 @@
 import { Component, Input } from '@angular/core';
+import { Video } from '../../models/video.class';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor],
   templateUrl: './category.component.html',
   styleUrl: './category.component.scss'
 })
 export class CategoryComponent {
 
   @Input() categoryName: string = ""; 
+  @Input() categoryVideos: Video[] = []; 
     
 
 }
