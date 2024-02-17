688113f25cf45ccb174be7cbb23017ba01b1429d
SebSeitz <sebastian_seitz13@web.de>
2024-02-17 22:20:34 +0100

    added fields to video upload

--- src/app/videoupload/videoupload.component.ts
+++ src/app/videoupload/videoupload.component.ts
@@ -12,56 +12,31 @@ import { VideoService } from '../services/video.service';
   styleUrl: './videoupload.component.scss'
 })
 export class VideouploadComponent {
+  title: string;
+  description: string;
+  video_file: File;
+  poster_file: File;
+  created_at: Date;
+  genre: string;
 
-  constructor(private http: HttpClient, private videoService: VideoService) {
 
+  onTitleChanged(event: any) {
+    this.title = event.target.value;
   }
-  /*
-  onChangeFile(event: any) {
-    if (event.target.files.length > 0) {
-      const file = event.target.files[0];
-      const formData = new FormData();
-      formData.append('file', file); formData
-      this.http.post('http://127.0.0.1:8000/video/', formData).subscribe((res: any) => {
-        debugger
-      });
-    }
+  onDescriptionChanged(event: any) {
+    this.description = event.target.value;
   }
-  */
-
-  async uploadVideo(videoFile: File): Promise<any> {
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-        }
-    }
-
-    return fetch("http://127.0.0.1:8000/video/", requestOptions)
-    .then(response => response.json())
-    .then(result => {
-
-        console.log("result from backend");
-        console.log(result);
-        return result
-    })
-    .catch(error => {
-      console.log('error', error);
-      return new Video();
-    })
+  onVideofileChanged(event: any) {
+    this.video_file = event.target.files[0];
   }
-  
-
-  handleFileInput(event: any): void {
-    const videoFile: File = event.target.files[0];
-    this.uploadVideo(videoFile)
-      .then(() => {
-        // Call your function to refresh the list of videos or perform any other actions
-        this.videoService.getAllVideos();
-      })
-      .catch(error => {
-        // Handle the error, e.g., show an error message to the user
-      });
+  onPosterfileChanged(event: any) {
+    this.poster_file = event.target.files[0];
+  }
+  onDateChanged(event: any) {
+    this.created_at = event.target.getDate();
+  }
+  onGenreChanged(event: any) {
+    this.genre = event.target.value
   }
 
 }
